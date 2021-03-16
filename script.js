94dfde2b41b507cb4cf3ef601e4d566d7703da3b
Ohad <Ohad.Mor.Yosef@gmail.com>
2021-03-16 17:20:12 +0200

    Added SPAA, more jQuery functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// Settings array name from localStorage to a variable
 const MORE_INFO_COINS = "moreInfoCoins";
+
 // Change duration before fetching data from api in second
 const REFETCH_TIME = 5;
 
@@ -6,6 +8,7 @@ $(window).on("load", () => {
   // On load actions
   localStorage.setItem(MORE_INFO_COINS, JSON.stringify([]));
   const fileredCoinId = [];
+  const checkedCoinsArray = [];
 
   // On load function
   search();
@@ -39,7 +42,7 @@ $(window).on("load", () => {
   function coinDiv(coinData) {
     return `<div class="coin card card-body" id="${coinData.id.toLowerCase()}" style="width: 15rem">
     <div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox">
+    <input class="form-check-input myCheckBox" type="checkbox" id="${coinData.id.toLowerCase()}CheckBox">
     </div>
     <h5 class="card-title">${coinData.symbol.toUpperCase()}</h5>
     <p class="card-text">${coinData.name}</p>
@@ -54,21 +57,21 @@ $(window).on("load", () => {
     const coinInfo = $(`#coinInfo-${id}`);
     const moreInfoCoins = parsedLocalStorageCoinInfo();
     const foundCoinInfo = moreInfoCoins.find((coin) => coin.id === id);
-
     if (
       (foundCoinInfo &&
         new Date().getTime() - new Date(foundCoinInfo.time).getTime() >=
           1000 * REFETCH_TIME) ||
       !foundCoinInfo
-    )
+    ) {
       // Show spinner when loading data
       $(".botcontainer").append(
         `<div class="lds-dual-ring overlay" id="spinner"></div>`
       );
-    $.get(`https://api.coingecko.com/api/v3/coins/${id}`, (coin) => {
-      // Remove spinner after finishing
-      $("#spinner").remove();
-      if (coinInfo.html() == "") {
+      $.get(`https://api.coingecko.com/api/v3/coins/${id}`, (coin) => {
+        // Remove spinner after finishing
+        $("#spinner").remove();
+        // Cleans the div for new data to arrive
+        coinInfo.html("");
         $(coinInfo).append(`
           <img src="${coin.image.large}" class="symbol">
           <div>
@@ -77,9 +80,9 @@ $(window).on("load", () => {
         <h6>EUR - ${coin.market_data.current_price.eur}€</h6>
         </div>
         `);
-      }
-      writeToLocalStorage(coin);
-    });
+        writeToLocalStorage(coin);
+      });
+    }
     $(coinInfo).slideToggle("normal");
   };
 
@@ -142,10 +145,107 @@ $(window).on("load", () => {
         1000 * REFETCH_TIME
     ) {
       moreInfoCoins[foundCoinIndex].time = new Date().getTime();
+      // added updated prices
+      moreInfoCoins[foundCoinIndex].prices = {
+        ils: coin.market_data.current_price.ils,
+        usd: coin.market_data.current_price.usd,
+        eur: coin.market_data.current_price.eur,
+      };
     }
     return localStorage.setItem(MORE_INFO_COINS, JSON.stringify(moreInfoCoins));
   };
 
+  // Gets parsed array from loclStorage by using created variable
   const parsedLocalStorageCoinInfo = () =>
     JSON.parse(localStorage.getItem(MORE_INFO_COINS));
+
+  // Checks if checkbox is checked or not
+  $(document).on("click", ".myCheckBox", function (e) {
+    let tempCoin = e.target.parentElement.parentElement.id;
+    console.log(e.target.checked);
+    console.log(tempCoin);
+    if (e.target.checked == true) {
+      if (checkedCoinsArray.length < 5) {
+        console.log("Checkbox is checked.");
+        checkedCoinsArray.push(tempCoin);
+        console.log(checkedCoinsArray);
+      } else {
+        alert("didnt happen");
+        e.target.checked = false;
+      }
+    } else if (
+      e.target.checked == false &&
+      checkedCoinsArray.includes(tempCoin)
+    ) {
+      console.log("Checkbox is unchecked.");
+      checkedCoinsArray.splice(checkedCoinsArray.indexOf(tempCoin), 1);
+      console.log(checkedCoinsArray);
+    } else {
+      alert("didnt happen");
+      e.target.checked = false;
+    }
+  });
+
+  // Creating empty variables to store DOM info
+  let aboutPage = null;
+  let homePage = null;
+
+  // Switch to about
+  $("#aboutPage").on("click", function (e) {
+    // Clears checkedCoinsArray
+    checkedCoinsArray.splice(0, checkedCoinsArray.length);
+    // Checks if homePage and aboutPage variable are empty
+    if (homePage == null || aboutPage == null) {
+      homePage = $(".botcontainer").html();
+      // Actually creating aboutPage dynamically
+      $(".botcontainer").html(`  <div
+      id="about"
+      class="d-flex align-items-center justify-content-around mx-auto"
+    >
+      <div id="picture"></div>
+      <div id="info">
+        <h1>Hello</h1>
+        <h3>My name is Ohad and I'm 27 years old and I'm from Nahariya.</h3>
+        <h4>
+          My webstie lets the user check out the value of Cryptocurrency in USD,
+          ILS or EUR.
+        </h4>
+      </div>
+    </div>`);
+      aboutPage = $(".botcontainer").html();
+    }
+    // Checks if aboutPage is empty and if aboutPage content isnt on screen
+    else if (aboutPage != null && aboutPage != $(".botcontainer").html()) {
+      $(".botcontainer").empty();
+      $(".botcontainer").append(aboutPage);
+    }
+    console.log(checkedCoinsArray);
+  });
+
+  // Switch to home
+  $("#home").on("click", function (e) {
+    // Checks if aboutPage variable is empty
+    if (homePage == null) {
+      homePage = $(".botcontainer").html();
+    }
+    // Check if homePage isnt empty and if homePage content isnt on screen
+    else if (homePage != null && homePage != $(".botcontainer").html()) {
+      $(".botcontainer").empty();
+      $(".botcontainer").append(homePage);
+    }
+    // Check if aboutPage was built and if homePage content isnt on screen
+    else if (aboutPage == null && homePage != $(".botcontainer").html()) {
+      $(".botcontainer").empty();
+      $(".botcontainer").append(homePage);
+    }
+  });
+  // ===========SPA=================
+
+  // Fill here
+  // function load() {
+  //   $(document).on({
+  //     ajaxStart: function () {},
+  //     ajaxStop: function () {},
+  //   });
+  // }
 });
